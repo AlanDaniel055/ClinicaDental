f0e1e6e2e8ba22c8a79b3a8ed46bba474933c0e2
Bondleton <itzelf182@gmail.com>
2024-09-22 17:58:06 -0600

    Esqueleto para la BD

---
+++ consultorio_api/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from consultorio_api.models import *
+
+admin.site.register(Profiles)
---
+++ consultorio_api/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authentication import TokenAuthentication
+from django.contrib.auth.models import AbstractUser, User
+from django.conf import settings
+
+class BearerTokenAuthentication(TokenAuthentication):
+    keyword = u"Bearer"
+
+
+class Profiles(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, default=None)
+    creation = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    update = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return "Perfil del usuario "+self.usuario.first_name+" "+self.usuario.last_name
