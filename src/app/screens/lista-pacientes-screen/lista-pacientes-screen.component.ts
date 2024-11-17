fe4534ad24b001384fa45ba3567df7d3d6780e1f
Bondleton <itzelf182@gmail.com>
2024-11-17 07:17:00 -0600

    Listado de pacientes, view doctor

--- src/app/screens/lista-pacientes-screen/lista-pacientes-screen.component.ts
+++ src/app/screens/lista-pacientes-screen/lista-pacientes-screen.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PacientesService } from 'src/app/services/pacientes.service';
 
 @Component({
   selector: 'app-lista-pacientes-screen',
   templateUrl: './lista-pacientes-screen.component.html',
   styleUrls: ['./lista-pacientes-screen.component.scss']
 })
-export class ListaPacientesScreenComponent {
+export class ListaPacientesScreenComponent implements OnInit {
+  listaPacientes: any[] = []; // Arreglo para almacenar los datos de los pacientes
+  loading: boolean = true; // Indicador de carga
+  error: boolean = false; // Indicador de error
 
+  constructor(private pacientesService: PacientesService) {}
+
+  ngOnInit(): void {
+    this.obtenerPacientes(); // Llamamos al método para obtener los datos
+  }
+
+  obtenerPacientes() {
+    this.pacientesService.obtenerListaPacientes().subscribe({
+      next: (resp) => {
+        // Mapeo de los datos para obtener el nombre completo
+        this.listaPacientes = resp.map((paciente: any) => ({
+          nombre: `${paciente.user.first_name} ${paciente.user.last_name} ${paciente.apellido_materno}`,
+          //photoFileName: paciente.photoFileName || ''
+          // TODO : checar lo de la ruta de imagen
+        }));
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error('Error al obtener la lista de pacientes:', error);
+        this.error = true;
+        this.loading = false;
+      }
+    });
+  }
 }
